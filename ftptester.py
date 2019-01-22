ab3b5eb93f41752b1b852a7942018fe98cabb79c
renaud gaudin <rgaudin@gmail.com>
2019-01-22 14:27:23 +0000

    updated tester to match worker behavior

--- ftptester.py
+++ ftptester.py
@@ -2,160 +2,63 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4 nu
 
+import os
 import sys
-import json
-import ftplib
-from time import sleep
-from pathlib import Path
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError
+import subprocess
 
-from typing import Optional
+import requests
 
 
-class Operation:
-    """Base class for all operations
-
-    Attributes:
-        success (:obj:`bool`, optional): task is successful or not
-        std_out (:obj:`bytes`, optional): std_out
-        error (:obj:`Error`, optional): error of the operation
-    """
-
-    name: str = "Operation Base Class"
-
-    def __init__(self):
-        self.success: Optional[bool] = None
-        self.std_out: Optional[bytes] = None
-        self.error: Optional[Error] = None
-
-    @property
-    def log(self) -> {}:
-        result = {"success": self.success, "name": self.name}
-
-        if self.std_out is not None:
-            result["std_out"] = self.std_out.decode()
-        if self.error is not None:
-            result["error"] = {
-                "domain": self.error.domain,
-                "code": self.error.code,
-                "stderr": self.error.stderr.decode()
-                if self.error.stderr is not None
-                else None,
-            }
-        return result
-
-    def execute(self):
-        pass
+def get_token(url, username, password):
+    req = requests.post(
+        url="{api}/auth/authorize".format(api=url),
+        headers={
+            "username": username,
+            "password": password,
+            "Content-type": "application/json",
+        },
+    )
+    req.raise_for_status()
+    return req.json().get("access_token"), req.json().get("refresh_token")
 
 
-class Error(Exception):
-    """Operation Error
+def main(api_url, warehouse_uri, username, password, fpath):
 
-    Attributes:
-        domain (optional): a code to identify which module produced the error
-        code (optional): a code to identify the error if error occurred
-        message (:obj:`str`, optional): a message to describe the error if error occurred
-        stderr (:obj:`bytes`, optional): stderr
-    """
+    # get token
+    access_token, refresh_token = get_token(api_url, username, password)
+    print("access_token", access_token)
 
-    def __init__(
-        self,
-        domain: Optional[str],
-        code: Optional[int] = None,
-        message: Optional[str] = None,
-        stderr: Optional[bytes] = None,
-    ):
-        self.domain = domain
-        self.code = code
-        self.message = message
-        self.stderr = stderr
+    args = [
+        "curl",
+        "--append",
+        "--connect-timeout",
+        "60",
+        "--continue-at",
+        "-",
+        "--insecure",
+        "--ipv4",
+        "--retry-connrefused",
+        "--retry-delay",
+        "60",
+        "--retry",
+        "20",
+        "--stderr",
+        "-",
+        "--user",
+        "{user}:{passwd}".format(user=username, passwd=access_token),
+        "--upload-file",
+        str(fpath),
+        warehouse_uri,
+    ]
 
-
-class Upload(Operation):
-    """ Upload zim files to server """
-
-    name = "Upload Zim File"
-
-    def __init__(
-        self,
-        zim_files_dir: Path,
-        dispatcher_host: str,
-        warehouse_host: str,
-        warehouse_command_port: int,
-        username: str,
-        password: str,
-    ):
-        super().__init__()
-        self.zim_files_dir: Path = zim_files_dir
-
-        self.dispatcher_host: str = dispatcher_host
-        self.warehouse_host: str = warehouse_host
-        self.warehouse_command_port: int = warehouse_command_port
-        self.username: str = username
-        self.password: str = password
-        self.token = None
-
-    def execute(self):
-        try:
-            self._get_token()
-            for path in self.zim_files_dir.iterdir():
-                if path.is_file() and path.suffix == ".zim":
-                    self._safe_upload(path)
-                    path.unlink()
-            try:
-                self.zim_files_dir.rmdir()
-            except OSError:
-                pass
-            self.success = True
-        except HTTPError as e:
-            self.success = False
-            self.error = Error("upload.token.HTTPError", e.code, str(e))
-        except ConnectionRefusedError as e:
-            self.success = False
-            self.error = Error("upload.ftp.ConnectionRefusedError", message=str(e))
-
-    def _get_token(self):
-        url = "https://{host}/auth/authorize".format(host=self.dispatcher_host)
-        headers = {"username": self.username, "password": self.password}
-        request = Request(url, headers=headers, method="POST")
-
-        with urlopen(request, timeout=30) as response:
-            response_json = json.loads(response.read(), encoding="utf-8")
-            self.token = response_json["access_token"]
-
-    def _safe_upload(self, path: Path):
-        retries = 1
-        error = None
-        while retries > 0:
-            try:
-                self._upload(path)
-            # except ConnectionRefusedError as exp:
-            except Exception as exp:  # ConnectionRefusedError ?
-                print("… failed.")
-                error = exp
-                retries -= 1
-                sleep(5)
-            else:
-                return
-        raise error or Exception("")
-
-    def _upload(self, path: Path):
-        print("Uploading {}…".format(path.name))
-        with ftplib.FTP() as ftp:
-            ftp.connect(self.warehouse_host, self.warehouse_command_port, timeout=30)
-            ftp.login(self.username, self.token)
-            with open(path, "rb") as file:
-                ftp.storbinary("STOR {}".format(path.name), file)
+    subprocess.run(args)
 
 
 if __name__ == "__main__":
-    uploader = Upload(
-        Path(sys.argv[1]),
-        "api.demo.plug.kiwix.org",
-        "warehouse.demo.plug.kiwix.org",
-        21,
-        "admin",
-        "admin_pass",
+    main(
+        api_url="https://api.cardshop.hotspot.kiwix.org",
+        warehouse_uri="ftp://download.cardshop.hotspot.kiwix.org:2221",
+        username="",
+        password="",
+        fpath=os.path.abspath(sys.argv[1]),
     )
-    uploader.execute()
