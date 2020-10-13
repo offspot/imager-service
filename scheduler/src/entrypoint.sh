38ce571abc06e1e742e51b268ccb78c3d81dceb4
renaud gaudin <rgaudin@gmail.com>
2020-10-13 13:45:06 +0000

    reloading crontab file after starting cron

--- scheduler/src/entrypoint.sh
+++ scheduler/src/entrypoint.sh
@@ -9,5 +9,10 @@ declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /contai
 echo "start cron"
 cron
 
+sleep 2
+
+# reload crontab
+crontab /etc/cron.d/scheduler-cron
+
 echo "run parent's entrypoint"
 exec /entrypoint.sh "$@"
