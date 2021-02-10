9f19a4b7e488945f0dcc4e20440602196ed0bf35
renaud gaudin <rgaudin@gmail.com>
2021-02-10 15:26:56 +0000

    removed obsolete code

--- rabbit/docker-cardshop-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# rewrite the rabbitmq config file with URL from ENV
-echo "rewriting rabbitmq.conf with CARDSHOP_API_URL (${CARDSHOP_API_URL})"
-sed -i "s#CARDSHOP_API_URL#${CARDSHOP_API_URL}#g" /etc/rabbitmq/rabbitmq.conf
-
-# continue with regular entry-point
-echo "passing over to main entrypoint"
-exec docker-entrypoint.sh "$@"
