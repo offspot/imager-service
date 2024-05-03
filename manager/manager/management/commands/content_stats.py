f8781ab395b7a5cd3c09e67d226bb092079ddd38
renaud gaudin <reg@rskg.org>
2024-05-03 16:28:12 +0000

    mgmt command for ZIM usage

---
+++ manager/manager/management/commands/content_stats.py
@@ -0,0 +1,46 @@
+import copy
+import datetime
+import json
+import logging
+import statistics
+from collections import OrderedDict
+from pprint import pprint
+
+from django.core.management.base import BaseCommand
+
+from manager.models import Configuration
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Get info content usage stats"
+
+    def handle(self, *args, **options):  # noqa: ARG002
+        content: dict[str, int] = {}
+
+        qs = Configuration.objects.all()
+
+        self.stdout.write(f"looping through {qs.count()} configurations")
+
+        for config in qs:
+
+            for zimid in config.content_zims:
+                if zimid not in content:
+                    content[zimid] = 1
+                else:
+                    content[zimid] += 1
+
+        content = OrderedDict(
+            sorted(list(content.items()), key=lambda item: item[1], reverse=True)
+        )
+
+        top = max(content.values())
+
+        for zimid, nb in content.items():
+            if nb < top // 10:
+                break
+            print(f"{zimid},{nb}")
+
+        self.stdout.write("")
+        self.stdout.write(self.style.SUCCESS("done"))
