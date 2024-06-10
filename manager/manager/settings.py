15272af73e6d07e668743dcbb8a2b9ec1f16d5e7
renaud gaudin <reg@rskg.org>
2024-06-10 11:19:24 +0000

    Fixed #425: bundling kiwix-android 3.11.0

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -235,7 +235,7 @@ OFFSPOT_LANGUAGES = [
 # must be manually updated from time to time
 KIWIX_READERS_SOURCES = {
     "windows": "https://download.kiwix.org/release/kiwix-desktop/kiwix-desktop_windows_x64_2.3.1-2.zip",
-    "android": "https://download.kiwix.org/release/kiwix-android/kiwix-3.10.1.apk",
+    "android": "https://download.kiwix.org/release/kiwix-android/kiwix-3.11.0.apk",
     "macos": "https://download.kiwix.org/release/kiwix-macos/kiwix-macos_3.3.0.dmg",
     "linux": "https://download.kiwix.org/release/kiwix-desktop/"
     + "kiwix-desktop_x86_64_2.3.1-4.appimage",
