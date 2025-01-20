8ecd65f9a0f700954cd6fc337003165564edcc04
rgaudin <reg@rskg.org>
2025-01-20 16:17:17 +0000

    include kiwix macOS 3.7.1

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -236,7 +236,7 @@ OFFSPOT_LANGUAGES = [
 KIWIX_READERS_SOURCES = {
     "windows": "https://download.kiwix.org/release/kiwix-desktop/kiwix-desktop_windows_x64_2.4.1.zip",
     "android": "https://download.kiwix.org/release/kiwix-android/kiwix-3.12.0.apk",
-    "macos": "https://download.kiwix.org/release/kiwix-macos/kiwix-macos_3.6.0.dmg",
+    "macos": "https://download.kiwix.org/release/kiwix-macos/kiwix-macos_3.7.1.dmg",
     "linux": "https://download.kiwix.org/release/kiwix-desktop/"
     + "kiwix-desktop_x86_64_2.3.1-4.appimage",
 }
