06d038059b47fd1562a51eade983a08c16181353
rgaudin <reg@rskg.org>
2025-11-24 10:38:43 +0000

    Fixed #498: bundled macOS kiwix 3.11.0

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -239,7 +239,7 @@ KIWIX_READERS_SOURCES = {
     "windows": "https://download.kiwix.org/release/kiwix-desktop/kiwix-desktop_windows_x64_2.4.1.zip",
     "android": "https://mirror.download.kiwix.org/release/kiwix-android/"
     "org.kiwix.kiwixmobile.standalone-3.14.0.apk",
-    "macos": "https://download.kiwix.org/release/kiwix-macos/kiwix-macos_3.10.1.dmg",
+    "macos": "https://download.kiwix.org/release/kiwix-macos/kiwix-macos_3.11.0.dmg",
     "linux": "https://download.kiwix.org/release/kiwix-desktop/"
     + "kiwix-desktop_x86_64_2.3.1-4.appimage",
 }
