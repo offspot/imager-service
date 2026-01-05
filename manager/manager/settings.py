0e7e23df2f9b490573e661fb57da5552dd88a8ca
rgaudin <reg@rskg.org>
2026-01-05 11:08:57 +0000

    Including kiwix-desktop 2.5.1 for Windows and Linux
    Fixes #501

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -238,12 +238,12 @@ OFFSPOT_LANGUAGES = [
 # All of them will be added to the builder if use selects the appr. option
 # must be manually updated from time to time
 KIWIX_READERS_SOURCES = {
-    "windows": "https://download.kiwix.org/release/kiwix-desktop/kiwix-desktop_windows_x64_2.4.1.zip",
+    "windows": "https://download.kiwix.org/release/kiwix-desktop/kiwix-desktop_windows_x64_2.5.1.zip",
     "android": "https://mirror.download.kiwix.org/release/kiwix-android/"
     "org.kiwix.kiwixmobile.standalone-3.14.0.apk",
     "macos": "https://download.kiwix.org/release/kiwix-macos/kiwix-macos_3.11.0.dmg",
     "linux": "https://download.kiwix.org/release/kiwix-desktop/"
-    + "kiwix-desktop_x86_64_2.3.1-4.appimage",
+    + "kiwix-desktop_x86_64_2.5.1.appimage",
 }
 
 
