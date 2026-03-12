b7547291c7b913f809b26adeafc4ad6bd950f664
rgaudin <reg@rskg.org>
2026-03-12 15:34:56 +0000

    fixup! Using base-image 1.4.2 with Captive portal fix

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -225,7 +225,7 @@ LANGUAGES = [
 ]
 
 BASE_IMAGE_URL = os.getenv("BASE_IMAGE_URL", "1.4.2")
-BASE_IMAGE_ROOTFS_SIZE = int(os.getenv("BASE_IMAGE_ROOTFS_SIZE", "3472883712"))
+BASE_IMAGE_ROOTFS_SIZE = int(os.getenv("BASE_IMAGE_ROOTFS_SIZE", "3716153344"))
 OFFSPOT_TLD = os.getenv("OFFSPOT_TLD", "hotspot")
 WIFI_PROFILE = os.getenv("WIFI_PROFILE", WIFI_PROFILES[0])
 # should utilmately be supported by offspot-config
