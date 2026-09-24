3bf74d4d147a6f89d186f7f9bcbbdd2fb30e80c4
rgaudin <reg@rskg.org>
2026-09-24 14:26:14 +0000

    Added sample worker script

---
+++ workers/worker-restart.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# imager worker start/restart script
+# stop worker with docker stop imager-worker
+
+USERNAME="worker"
+PASSWORD="notset"
+CONTAINER="imager-worker"
+IMAGE="ghcr.io/offspot/imager-worker:imager"
+S3_ACCESS_KEY="notset"
+S3_SECRET_KEY="notset"
+SSH_KEY_PATH="/etc/ssh/imager-worker"
+
+if [ -f /etc/imager.config ]; then
+    source /etc/imager.config
+fi
+
+# already running?
+docker ps |grep $CONTAINER |awk '{print $1}' | while read line ; do
+    echo ">stopping worker container $line"
+    docker stop $line
+    echo ">removing worker container $line"
+    docker rm $line
+done
+
+docker stop $CONTAINER
+docker rm $CONTAINER
+
+echo ">pulling image $IMAGE…"
+docker pull $IMAGE
+
+echo ">starting creator worker"
+docker run \
+    --name $CONTAINER \
+    -v /data/imager:/data \
+    -v /data/cache:/cache \
+    -v $SSH_KEY_PATH:/etc/ssh/imager-worker \
+    -v /etc/ssh/known_hosts:/root/.ssh/known_hosts \
+    -e USERNAME="$USERNAME" \
+    -e PASSWORD="$PASSWORD" \
+    -e S3_ACCESS_KEY="$S3_ACCESS_KEY" \
+    -e S3_SECRET_KEY="$S3_SECRET_KEY" \
+    -e CARDSHOP_API_URL="https://api.imager.kiwix.org" \
+    --privileged \
+    --restart unless-stopped \
+    --detach \
+    $IMAGE
